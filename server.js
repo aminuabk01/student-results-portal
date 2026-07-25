4b0661e34a9c9b0e93fab20cf69e6bfe3e788f3f
Aminu Abubakar <aminuabubakar6361@gmail.com>
2026-07-25 13:17:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use(session({
 
 // Routes
 app.get('/', (req, res) => res.redirect('/login'));
-app.use('/', require('./routes/setup'));
 app.use('/', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/student', studentRoutes);
